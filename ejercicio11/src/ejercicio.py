48c6ef4051ecaac4ba727e48459a39fdaee6c2c3
nicolasfraga01 <61249656+nicolasfraga01@users.noreply.github.com>
2020-10-04 12:42:37 +0200

    Update ejercicio.py

--- ejercicio11/src/ejercicio.py
+++ ejercicio11/src/ejercicio.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 
 import rospy
@@ -25,31 +24,3 @@ rospy.init_node('distmin_laser_11')
 sub=rospy.Subscriber('/kobuki/laser/scan',LaserScan,callback)
 rospy.Rate(10)
 rospy.spin()
-=======
-#!/usr/bin/env python
-
-import rospy
-
-from sensor_msgs.msg import LaserScan
-
-
-def callback(msg):
- 
-    distancia_izq=msg.ranges[719]
-    distancia_der=msg.ranges[0]
-    distancia_del=msg.ranges[360]
-    vector=[distancia_izq,distancia_der,distancia_del]
-    if min(vector)==distancia_izq:
-        print "Distancia minima--> 180 grados: ",msg.ranges[719]
-    elif min(vector)==distancia_der:
-        print "Distancia minima--> 0 grados: ",msg.ranges[0]
-    elif min(vector)==distancia_del:
-        print "Distancia minima--> 90 grados: ", msg.ranges[360]
-    
-
-
-rospy.init_node('distmin_laser_11')
-sub=rospy.Subscriber('/kobuki/laser/scan',LaserScan,callback)
-rospy.Rate(10)
-rospy.spin()
->>>>>>> 6dedaaac996750cf26c04b68c4efbb52bd779f69
