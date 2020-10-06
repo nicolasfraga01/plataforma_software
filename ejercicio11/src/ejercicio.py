ce49ae7ddfa6ed9ef5fbe82b548bb27faa8e0b2f
nicolasfraga01 <61249656+nicolasfraga01@users.noreply.github.com>
2020-10-06 13:49:55 +0200

    Update ejercicio.py

--- ejercicio11/src/ejercicio.py
+++ ejercicio11/src/ejercicio.py
@@ -7,16 +7,16 @@ from sensor_msgs.msg import LaserScan
 
 def callback(msg):
  
-    distancia_izq=msg.ranges[719]
+    distancia_izq=msg.ranges[len(msg.ranges)-1]
     distancia_der=msg.ranges[0]
-    distancia_del=msg.ranges[360]
+    distancia_del=msg.ranges[len(msg.ranges)/2]
     vector=[distancia_izq,distancia_der,distancia_del]
     if min(vector)==distancia_izq:
-        print "Distancia minima--> 180 grados: ",msg.ranges[719]
+        print "Distancia minima--> 180 grados(izquierda): ",distancia_izq
     elif min(vector)==distancia_der:
-        print "Distancia minima--> 0 grados: ",msg.ranges[0]
+        print "Distancia minima--> 0 grados(derecha): ",distancia_der
     elif min(vector)==distancia_del:
-        print "Distancia minima--> 90 grados: ", msg.ranges[360]
+        print "Distancia minima--> 90 grados(frontal): ", distancia_del
     
 
 
